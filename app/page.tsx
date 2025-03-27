29992de0455e0bdffcdbdc54128cc401b422492b
Bhuv <bhuv059@gmail.com>
2025-03-27 16:59:49 +0100

    Removed header file

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import Msgbox from "@/components/Msgbox";
-import Header from "@/components/Header";
 
 export default function Home() {
     return (
         <div className="py-20">
-            {/*<Header />*/}
+
             <Msgbox />
         </div>
     )
